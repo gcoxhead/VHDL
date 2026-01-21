b3c7068b166478d765222676ac1d8bf454bea9ef
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2026-01-21 21:37:56 +0000

    Update -- Testbench for OR gate.vhd

--- -- Testbench for OR gate.vhd
+++ -- Testbench for OR gate.vhd
@@ -20,35 +20,41 @@ signal a_in, b_in, q_out: std_logic;
 
 begin
 
-  -- Connect DUT
+  -- Instantiate the DUT (Device Under Test)
   DUT: or_gate port map(a_in, b_in, q_out);
 
   process
   begin
+    -- Test case 1: 0 OR 0
     a_in <= '0';
     b_in <= '0';
     wait for 1 ns;
     assert(q_out='0') report "Fail 0/0" severity error;
-  
+    
+    -- Test case 2: 0 OR 1
     a_in <= '0';
     b_in <= '1';
     wait for 1 ns;
     assert(q_out='1') report "Fail 0/1" severity error;
 
+    -- Test case 3: 1 OR unknown (x)
     a_in <= '1';
     b_in <= 'X';
     wait for 1 ns;
     assert(q_out='1') report "Fail 1/X" severity error;
 
+    -- Tes case 4: 1 OR 1 
     a_in <= '1';
     b_in <= '1';
     wait for 1 ns;
     assert(q_out='1') report "Fail 1/1" severity error;
     
-    -- Clear inputs
+    -- Clear inputs back to 0
     a_in <= '0';
     b_in <= '0';
-
+    wait for 1 ns; -- Ensure signal has time to settle
+    
+    -- End of tests
     assert false report "Test done." severity note;
     wait;
   end process;
